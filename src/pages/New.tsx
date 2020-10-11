1e49b0d37cb33cc449965cb19e17ad643600a2d3
Mat Sz <contact@matsz.dev>
2020-10-11 16:18:12 +0200

    added monaco editor to the new page

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Editor from '@monaco-editor/react';
 
 export const New: React.FC = () => {
-  return <div></div>;
+  return <Editor height="100%" theme="dark" />;
 };
